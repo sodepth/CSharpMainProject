aaa9390937a98ac49dd6a43d11c658152db4b62e
sodepth <Prifaryt@gmail.com>
2025-08-01 23:42:52 +0300

    upd maxHeight

--- Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
+++ Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
@@ -25,15 +25,9 @@ namespace Model.Runtime.Projectiles
             
             float localHeight = 0f;
             float totalDistance = _totalDistance;
-
-            ///////////////////////////////////////
-            // Insert you code here
-            ///////////////////////////////////////
-
-
-            ///////////////////////////////////////
-            // End of the code to insert
-            ///////////////////////////////////////
+            float maxHeight = totalDistance * 0.6f;
+            
+            localHeight = maxHeight * (-(t * 2 - 1) * (t * 2 - 1) + 1);
             
             Height = localHeight;
             if (time > StartTime + _timeToTarget)
